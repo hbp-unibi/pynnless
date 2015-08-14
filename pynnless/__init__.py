6fc142c99b9ccdaca636c28a20cac1ce28e05bb1
Andreas Stöckel <astoecke@techfak.uni-bielefeld.de>
2015-08-14 11:55:30 +0200

    Import PyNNLess class into top-level package namespace

--- pynnless/__init__.py
+++ pynnless/__init__.py
@@ -16,6 +16,8 @@
 #   You should have received a copy of the GNU General Public License
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+# Import the "PyNNLess" class to the top-level package namespace
+from pynnless import PyNNLess
+
+# Current version of the "PyNNLess" wrapper
 __version__ = "1.0.0"
-
-
