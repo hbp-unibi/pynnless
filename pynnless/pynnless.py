9edcdcf84bf6f282fd0512b6f46733d4de6a57a6
Andreas Stöckel <astoecke@techfak.uni-bielefeld.de>
2015-10-24 22:51:37 +0200

    Spikey only supports 192 neurons at the moment
    This limitation was never triggered by PyNAM as spike sources were counted
    as neuron.

--- pynnless/pynnless.py
+++ pynnless/pynnless.py
@@ -1057,7 +1057,7 @@ class PyNNLess:
         elif simulator == "nmmc1":
             res["max_neuron_count"] = 3 * 48 * 16 * 128 # TODO: Actual board size
         elif simulator == "spikey":
-            res["max_neuron_count"] = 384
+            res["max_neuron_count"] = 192
             res["shared_parameters"] = ["v_rest", "v_reset", "v_thresh",
                     "e_rev_I"]
 
